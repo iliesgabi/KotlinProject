50a41e112da6bf59c55c7a78b4211e728b4001da
iliesgabi <iliesgabi99@yahoo.ro>
2020-03-08 14:50:30 +0200

    tema1++

--- Tema1/app/src/main/java/com/example/tema1/Tema1.kt
+++ Tema1/app/src/main/java/com/example/tema1/Tema1.kt
@@ -29,16 +29,6 @@ fun cardinal(unghi:Int):String{
     }
 }
 
-fun numarVocale(mesaj: String):Int{
-    val vocale:String="aeiouAEIOU"
-    var contor:Int=0;
-    for(i in 0..mesaj.length-1){
-        if (vocale.indexOf(mesaj[i],0)!=-1)
-            contor++;
-    }
-    return contor;
-}
-
 class RectangularShape(var x: Int, var y: Int, var with: Int, var heigh: Int, var color: Int) {
     fun measure(){}
     fun render(){}
@@ -55,5 +45,134 @@ fun validateShape(shape: RectangularShape): Boolean {
         else -> return true
     }
 }
+
+
+fun initShape(shape: RectangularShape?){
+    shape?.apply{
+        x = 10
+        y = 20
+        with = 100
+        heigh = 200
+        color = 0xFF0066
+    } ?: throw IllegalArgumentException()
+}
+
+fun numarVocale(mesaj: String):Int{
+    val vocale:String="aeiouAEIOU"
+    var contor:Int=0;
+    for(chr in mesaj){
+        if (chr in "aeiouAEIOU")
+            contor++;
+    }
+    return contor;
+}
+
+val data=listOf(4, 6, 34, 9, 2, 4, 7)
+fun printData(){
+    for(it in data)
+        print("$it ")
+}
+
+fun printReverse() {
+    for (i in data.size - 1 downTo 0)
+        print(data[i])
+}
+
+val noDuplicate=data.distinct();
+fun printNoDuplicate() {
+    for (it in noDuplicate)
+        print("$it ")
+}
+
+fun print3() {
+    for (i in 0..2)
+        print(data[i])
+}
+
+fun printPositive() {
+    var ok = false;
+    for (it in data)
+        if (it < 0)
+            ok = true;
+    if (!ok)
+        for (it in data)
+            print("$it ")
+}
+
+fun printSqrt() {
+    for (it in data) {
+        val aux = Math.sqrt(it.toDouble())
+        print("$aux ")
+    }
+}
+
+fun Int.isEven(): Boolean = this % 2 == 0
+fun printEven() {
+    for (it in data)
+        if (it.isEven())
+            print("$it ")
+}
+
+fun printOddIndex() {
+    for (i in 0..data.size - 1)
+        if (!data[i].isEven())
+            print("$i ")
+}
+
+fun Int.isPrime(): Boolean{
+    return (2..this/2).none { this % it == 0 }
+}
+
+fun printPrime() {
+    for (it in data)
+        if (it.isPrime())
+            print("$it ")
+}
+
+fun printLastPrime() {
+    var prim = 0;
+    for (it in data)
+        if (it.isPrime())
+            prim = it;
+    if (prim == 0)
+        print("nu exista numere prime")
+    else
+        print("ultimul numar prim din sir este $prim")
+}
+
+data class Student(val name: String, val address: String, val grade: Int)
+val students = listOf(
+    Student("John", "Boston", 6),
+    Student("Jacob", "Baltimore", 2),
+    Student("Edward", "New York", 7),
+    Student("William", "Providence", 6),
+    Student("Alice", "Philadelphia", 4),
+    Student("Robert", "Boston", 7),
+    Student("Richard", "Boston", 10),
+    Student("Steven", "New York", 3) )
+
+
+fun printStudents() {
+    for (it in students)
+        print(it)
+}
+
+fun printSortedStudents() {
+    val sortedStudents = students.sortedBy { it.name }
+    for (it in sortedStudents)
+        print(it)
+}
+
+fun printName() {
+    val sortedStudents = students.sortedBy { it.name }
+    for (it in sortedStudents)
+        print(it.name)
+}
+
+val comp = compareBy<Pair<Int, String>>({it.first}, {it.second})
+
+
+
+
 
 
